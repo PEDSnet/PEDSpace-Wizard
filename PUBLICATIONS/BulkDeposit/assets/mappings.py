5c505e4afcee92fa641d890fb7a4bd598ec0af27
Annabel Pinkney <157635500+abelpink@users.noreply.github.com>
2026-01-14 11:54:23 -0500

    Update PUBLICATIONS wizard

---
+++ PUBLICATIONS/BulkDeposit/assets/mappings.py
@@ -0,0 +1,104 @@
+METADATA_TO_DUBLIN_XML_MAPPING = {
+    "title": {"element": "title", "qualifier": "none"},
+
+    "date_published": {"element": "date", "qualifier": "issued"},
+
+    "Creator_01": {"element": "contributor", "qualifier": "author"},
+    "Creator_02": {"element": "contributor", "qualifier": "author"},
+    "Creator_03": {"element": "contributor", "qualifier": "author"},
+    "Creator_04": {"element": "contributor", "qualifier": "author"},
+    "Creator_05": {"element": "contributor", "qualifier": "author"},
+    "Creator_06": {"element": "contributor", "qualifier": "author"},
+    "Creator_07": {"element": "contributor", "qualifier": "author"},
+    "Creator_08": {"element": "contributor", "qualifier": "author"},
+    "Creator_09": {"element": "contributor", "qualifier": "author"},
+    "Creator_10": {"element": "contributor", "qualifier": "author"},
+    "Creator_11": {"element": "contributor", "qualifier": "author"},
+    "Creator_12": {"element": "contributor", "qualifier": "author"},
+    "Creator_13": {"element": "contributor", "qualifier": "author"},
+    "Creator_14": {"element": "contributor", "qualifier": "author"},
+    "Creator_15": {"element": "contributor", "qualifier": "author"},
+    "Creator_16": {"element": "contributor", "qualifier": "author"},
+    "Creator_17": {"element": "contributor", "qualifier": "author"},
+    "Creator_18": {"element": "contributor", "qualifier": "author"},
+    "Creator_19": {"element": "contributor", "qualifier": "author"},
+    "Creator_20": {"element": "contributor", "qualifier": "author"},
+    "Creator_21": {"element": "contributor", "qualifier": "author"},
+    "Creator_22": {"element": "contributor", "qualifier": "author"},
+    "Creator_23": {"element": "contributor", "qualifier": "author"},
+    "Creator_24": {"element": "contributor", "qualifier": "author"},
+    "Creator_25": {"element": "contributor", "qualifier": "author"},
+    "Creator_26": {"element": "contributor", "qualifier": "author"},
+    "Creator_27": {"element": "contributor", "qualifier": "author"},
+
+    "Affiliation_01": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_02": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_03": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_04": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_05": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_06": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_07": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_08": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_09": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_10": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_11": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_12": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_13": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_14": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_15": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_16": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_17": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_18": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_19": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_20": {"element": "contributor", "qualifier": "other"},
+    "Affiliation_21": {"element": "contributor", "qualifier": "other"},
+
+    "Abstract": {"element": "description", "qualifier": "abstract"},
+    "Description": {"element": "description", "qualifier": "none"},
+    "Citation": {"element": "identifier", "qualifier": "citation"},
+    "DOI": {"element": "identifier", "qualifier": "doi"},
+    "Pubmed_link": {"element": "relation", "qualifier": "uri"},
+    "Lang": {"element": "language", "qualifier": "iso"},
+    "Rights": {"element": "rights", "qualifier": "none"},
+
+    "MeSH_01": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_02": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_03": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_04": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_05": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_06": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_07": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_08": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_09": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_10": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_11": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_12": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_13": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_14": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_15": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_16": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_17": {"element": "subject", "qualifier": "mesh"},
+    "MeSH_18": {"element": "subject", "qualifier": "mesh"},
+
+    "Publisher": {"element": "publisher", "qualifier": "none"},
+
+}
+
+METADATA_TO_DSPACE_XML_MAPPING = {
+    "Type": {"element": "entity", "qualifier": "type"}
+}
+
+DOMAIN_TO_THUMBNAIL_FILE_MAPPING = {
+   "Device": "CONCEPTSET.png",
+   "Diagnoses": "CONCEPTSET.png",
+   "Environmental and Socioeconomic Variables": "CONCEPTSET.png",
+   "Lab Results": "CONCEPTSET.png",
+   "Medications": "CONCEPTSET.png",
+   "Physiological Measurements": "CONCEPTSET.png",
+   "Procedures": "CONCEPTSET.png",
+   "Visits": "CONCEPTSET.png"
+}
+
+DOMAIN_TO_COLLECTION_MAPPING ={
+   "Scholarly Output Catalog": "20.500.14642/1271",
+}
